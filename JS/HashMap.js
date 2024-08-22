0d4e979c3ea72258e7e37c04dbf55efc0b132c27
Hank_0050358 <w610w610@gmail.com>
2024-08-22 15:28:16 +0800

    第二次CR報告

--- JS/HashMap.js
+++ JS/HashMap.js
@@ -1,33 +1,23 @@
-
 var HashMap = function () {
-    let map = {};
-    return {
-        put: function (k, v) {
-            map[k] = v;
-        },
-        keys: function () {
-            let keylist = [];
-            for (const key in map) {
-                keylist.push(key);
-            }
-            return keylist;
-
-        },
-        contains: function (k) {
-            for (const key in map) {
-                if (key == k) {
-                    return true;
-                }
-            }
-            return false;
-        },
-        get: function (k) {//*
-            return map[k];
-        },
-        clear: function () {
-            map = {};
-        }
-
-    }
-
-}
+  let map = {};
+  return {
+    put: function (k, v) {
+      map[k] = v;
+    },
+    keys: function () {
+      return Object.keys(map);
+    },
+    contains: function (k) {
+      if (k in map) {
+        return true;
+      }
+      return false;
+    },
+    get: function (k) {
+      return map[k];
+    },
+    clear: function () {
+      map = {};
+    },
+  };
+};
